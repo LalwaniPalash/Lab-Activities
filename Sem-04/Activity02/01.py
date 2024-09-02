e2da4b777eb06e639c6bb408f6ee577147ca2c58
Palash Lalwani <147582785+LalwaniPalash@users.noreply.github.com>
2024-09-02 11:28:41 +0530

    Create 01.py

---
+++ Sem-04/Activity02/01.py
@@ -0,0 +1,8 @@
+import numpy as np
+import array
+
+numpy_array = np.arange(1, 11)
+print("NumPy array:", numpy_array)
+
+array_array = array.array('i', range(1, 11))
+print("Array module array:", array_array)
