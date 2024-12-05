44556b151a4faabe74d64ec85f1e51c728845f32
Palash Lalwani <147582785+LalwaniPalash@users.noreply.github.com>
2024-12-05 23:29:37 +0530

    Create 06.py

---
+++ Sem-05/Data Science Lab/Activity01/06.py
@@ -0,0 +1,19 @@
+def count_occurrences_with_builtin(arr, element):
+    return arr.count(element)
+
+arr = [7, 3, 2, 1, 8, 6, 3, 3, 5]
+element = 3
+print(f"Occurrences of {element} using built-in function:", count_occurrences_with_builtin(arr, element))
+
+----------------
+
+def count_occurrences_without_builtin(arr, element):
+    count = 0
+    for item in arr:
+        if item == element:
+            count += 1
+    return count
+
+arr = [7, 3, 2, 1, 8, 6, 3, 3, 5]
+element = 3
+print(f"Occurrences of {element} without using built-in function:", count_occurrences_without_builtin(arr, element))
